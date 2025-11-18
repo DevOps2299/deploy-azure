f0251b2ace4efee453a6153046042ba8a09ee580
DevOps2299 <mathew.wasyliw@lambtoncollege.ca>
2025-11-17 23:16:14 -0500

    Message updated

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Azure VM via GitHub Actions! Booy!!!"
+    return "Hello from Azure VM via GitHub Actions! It actually worked!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
