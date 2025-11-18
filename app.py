1c68898d9f1a01cb5f59738b1d3278207651ea4b
DevOps2299 <mathew.wasyliw@lambtoncollege.ca>
2025-11-17 23:08:56 -0500

    Updated message, test workflow

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Azure VM via GitHub Actions!"
+    return "Hello from Azure VM via GitHub Actions! Booy!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
